a5293d3b9b2444d16a1ee144324676caa54d4888
Vedran Bilopavlović <vbilopav@gmail.com>
2021-05-21 12:18:10 +0200

    update unit tests

--- CompaniesWebSvetle/CompaniesDbTests/TestFixtures.cs
+++ CompaniesWebSvetle/CompaniesDbTests/TestFixtures.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Norm;
 using Npgsql;
@@ -14,17 +15,38 @@ namespace CompaniesDbTests
         public string TestConnection { get; set; }
         public string ConfigPath { get; set; }
         public string TestDatabaseName { get; set; }
+        public bool TestDatabaseFromTemplate { get; set; }
         public List<string> UpScripts { get; set; } = new();
         public List<string> DownScripts { get; set; } = new();
+        public bool UnitTestsUnderTransaction { get; set; }
+        public bool UnitTestsNewDatabaseFromTemplate { get; set; }
 
         public static Config Value { get; }
         public static string ConnectionString { get; }
-        
+
         static Config()
         {
             Value = new Config();
             var config = new ConfigurationBuilder().AddJsonFile("testsettings.json", false, false).Build();
             config.GetSection("TestSettings").Bind(Value);
+            if (Value.TestDatabaseFromTemplate && Value.UnitTestsNewDatabaseFromTemplate)
+            {
+                throw new ArgumentException(@"Configuration settings TestDatabaseFromTemplate=true and UnitTestsNewDatabaseFromTemplate=true doesn't make any sense.
+There is no point of creating a test database from a template and to do that again for each unit test.
+Set one of TestDatabaseFromTemplate or UnitTestsNewDatabaseFromTemplate to false.");
+            }
+            if (Value.UnitTestsNewDatabaseFromTemplate && Value.UnitTestsUnderTransaction)
+            {
+                throw new ArgumentException(@"Configuration settings UnitTestsNewDatabaseFromTemplate=true and UnitTestsUnderTransaction=true doesn't make any sense.
+There is no point of creating a new test database from a template for each test and then use transaction on a database where only one test runs.
+Set one of UnitTestsNewDatabaseFromTemplate or UnitTestsUnderTransaction to false.");
+            }
+            if (Value.UnitTestsNewDatabaseFromTemplate && (Value.UpScripts.Any() || Value.DownScripts.Any()))
+            {
+                throw new ArgumentException(@"Configuration settings UnitTestsNewDatabaseFromTemplate=true and up or down scripts (UpScripts, DownScripts) doesn't make any sense.
+Up or down scripts are only applied on a test database created for all tests.
+Set one of UnitTestsNewDatabaseFromTemplate or clear UpScripts and DownScripts.");
+            }
             string externalConnectionString = null;
             if (Value.ConfigPath != null && File.Exists(Value.ConfigPath))
             {
@@ -56,25 +78,48 @@ namespace CompaniesDbTests
             DropTestDatabase(connection);
         }
 
-        private static void CreateTestDatabase(NpgsqlConnection connection)
+        public static void CreateDatabase(NpgsqlConnection connection, string database, string template = null)
         {
-            void DoCreate() => connection.Execute($"create database {Config.Value.TestDatabaseName}");
+            void DoCreate() => connection.Execute($"create database {database}{(template == null ? "" : $" template {template}")}");
+            if (template != null)
+            {
+                connection.Execute(RevokeUsersCmd(template));
+            }
             try
             {
                 DoCreate();
             }
-            catch (PostgresException e) 
+            catch (PostgresException e)
             when (e.SqlState == "42P04") // 42P04=duplicate_database, see https://www.postgresql.org/docs/9.3/errcodes-appendix.html
             {
-                DropTestDatabase(connection);
+                DropDatabase(connection, database);
                 DoCreate();
             }
         }
 
-        private static void DropTestDatabase(NpgsqlConnection connection) => connection.Execute($@"
-            revoke connect on database {Config.Value.TestDatabaseName} from public;
-            select pg_terminate_backend(pid) from pg_stat_activity where datname = '{Config.Value.TestDatabaseName}' and pid <> pg_backend_pid();
-            drop database {Config.Value.TestDatabaseName};");
+        public static void DropDatabase(NpgsqlConnection connection, string database)
+        {
+            connection.Execute($@"
+            {RevokeUsersCmd(database)}
+            drop database {database};");
+        }
+
+        private static void CreateTestDatabase(NpgsqlConnection connection)
+        {
+            if (Config.Value.TestDatabaseFromTemplate)
+            {
+                CreateDatabase(connection, Config.Value.TestDatabaseName, connection.Database);
+            }
+            else
+            {
+                CreateDatabase(connection, Config.Value.TestDatabaseName);
+            }
+        }
+
+        private static void DropTestDatabase(NpgsqlConnection connection)
+        {
+            DropDatabase(connection, Config.Value.TestDatabaseName);
+        }
 
         private static void ApplyMigrations(NpgsqlConnection connection, List<string> scriptPaths)
         {
@@ -82,6 +127,14 @@ namespace CompaniesDbTests
             {
                 connection.Execute(File.ReadAllText(path));
             }
+        }
+
+        private static string RevokeUsersCmd(string database)
+        {
+            return $@"
+            revoke connect on database {database} from public;
+            select pg_terminate_backend(pid) from pg_stat_activity where datname = '{database}' and pid <> pg_backend_pid();
+            ";
         }
     }
 
@@ -95,16 +148,41 @@ namespace CompaniesDbTests
 
         protected PostgreSqlUnitTestFixture(PostgreSqlFixture fixture)
         {
-            Connection = fixture.Connection.CloneWith(fixture.Connection.ConnectionString);
-            Connection.Execute("begin");
+            if (Config.Value.UnitTestsNewDatabaseFromTemplate)
+            {
+                var dbName = string.Concat(Config.Value.TestDatabaseName, "_", Guid.NewGuid().ToString().Replace("-", "_"));
+                using var connection = new NpgsqlConnection(Config.ConnectionString);
+                PostgreSqlFixture.CreateDatabase(connection, dbName, connection.Database);
+                Connection = fixture.Connection.CloneWith(fixture.Connection.ConnectionString);
+                Connection.Open();
+                Connection.ChangeDatabase(dbName);
+            }
+            else
+            {
+                Connection = fixture.Connection.CloneWith(fixture.Connection.ConnectionString);
+                Connection.Open();
+            }
+
+            if (Config.Value.UnitTestsUnderTransaction)
+            {
+                Connection.Execute("begin");
+            }
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize", Justification = "XUnit")]
         public void Dispose()
         {
-            Connection.Execute("rollback");
+            if (Config.Value.UnitTestsUnderTransaction)
+            {
+                Connection.Execute("rollback");
+            }
             Connection.Close();
             Connection.Dispose();
+            if (Config.Value.UnitTestsNewDatabaseFromTemplate)
+            {
+                using var connection = new NpgsqlConnection(Config.ConnectionString);
+                PostgreSqlFixture.DropDatabase(connection, Connection.Database);
+            }
         }
     }
 }
